9b4369868269906a34de1933f90bf4cbde9bb4e7
lpitta <lpitta@gmail.com>
2019-02-12 13:01:35 +0000

    mbmbmnbmnb

---
+++ codigo.js
@@ -0,0 +1,6 @@
+
+  var num1, num2, sum;
+  num1 = prompt("Enter first number");
+  num2 = prompt("Enter second number");
+  sum = parseInt(num1) + parseInt(num2); // "+" means "add"
+  alert("Sum = " + sum);  // "+" means combine into a string
